1fe3c846759210c8a15e0ce5f281efee9019ef6e
Omkar Patil <omkarpatil290@gmail.com>
2024-06-07 19:45:05 +0530

    feat: dismiss alert

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../../javascript .js
 //= link_tree ../builds
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
